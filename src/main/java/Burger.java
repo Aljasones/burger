22ef45ba45b1f81af65ccc801521d904acb7a2bc
Aleksey <aljasones@yandex.ru>
2019-09-23 23:29:59 +0300

    1st version

---
+++ src/main/java/Burger.java
@@ -0,0 +1,14 @@
+public class Burger {
+    int id;
+    int priceInRubles;
+    String pictureUrl;
+    String name;
+    int weightInGram;
+    String description;
+    int kilocalories;
+    double proteinInGram;
+    double fatsInGram;
+    double carbohydrateInGram;
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Burger cheeseburger = new Burger();
+
+        cheeseburger.id = 2336;
+        cheeseburger.priceInRubles = 69;
+        cheeseburger.pictureUrl = "https://www.kfc.ru/foodorder/product/2336";
+        cheeseburger.name = "Чизбургер";
+        cheeseburger.weightInGram = 167;
+        cheeseburger.description = "Пряный горчичный соус, кетчуп, сочные стрипсы в оригинальной панировке, лук, сыр Чеддер, огурцы на пшеничной булочке с кукурузной посыпкой.";
+        cheeseburger.kilocalories = 237;
+        cheeseburger.proteinInGram = 13.2;
+        cheeseburger.fatsInGram = 8.6;
+        cheeseburger.carbohydrateInGram = 26.6;
+
+        Burger longer = new Burger();
+
+        longer.id = 1018;
+        longer.priceInRubles = 50;
+        longer.pictureUrl = "https://www.kfc.ru/foodorder/product/1018";
+        longer.name = "Лонгер BBQ";
+        longer.weightInGram = 107;
+        longer.description = "Сочная курочка, маринованные огурчики, лук и аппетитный соус барбекю! Спешите попробовать!";
+        longer.kilocalories = 236;
+        longer.proteinInGram = 9.7;
+        longer.fatsInGram = 7.1;
+        longer.carbohydrateInGram = 33.3;
+
+
+
+
+
+    }
+}
